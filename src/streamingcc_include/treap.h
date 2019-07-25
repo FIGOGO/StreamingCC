1b07851b09ac52c4f3c0b31bba272a8e56c4b189
Yan <songyan@iu.edu>
2019-07-25 14:26:24 -0400

    replace find by contains; passed cpplint check

--- src/streamingcc_include/treap.h
+++ src/streamingcc_include/treap.h
@@ -1,8 +1,11 @@
-#ifndef STREAMINGCC_TREAP_H
-#define STREAMINGCC_TREAP_H
+// Copyright 2019 Yan Song
+
+#ifndef SRC_STREAMINGCC_INCLUDE_TREAP_H_
+#define SRC_STREAMINGCC_INCLUDE_TREAP_H_
 
 #include <random>
 #include <ctime>
+#include <cstdlib>
 
 namespace streamingcc {
 
@@ -44,26 +47,30 @@ class Treap {
     void rotate_left(treeNode<T>* tn);
     void rotate_right(treeNode<T>* tn);
 
-    // Insertion below a treeNode
+    // Underlying insertion and searching
     treeNode<T>* insert(treeNode<T>* tn, T val);
+    bool contains(treeNode<T>* tn, T val);
 
     // Helper functions
     void assignPriority(treeNode<T>* tn);
     void updateHeight(treeNode<T>* tn);
     void adjustPriority(treeNode<T>* tn);
 
-public:
+    // Clear treap
+    void destroyRecursive(treeNode<T>* tn);
+
+ public:
     // Constructor
     Treap() {
         this->root = new treeNode<T>();
         this->_size = 0;
     }
-    // Deconstructor
-    ~Treap() {};
+    // Destructor
+    ~Treap() { destroyRecursive(this->root); }
 
-    void insert(T val) { insert(this->root, val); };
-    treeNode<T>* find(T val);
+    void insert(T val) { insert(this->root, val); }
+    bool contains(T val) { return contains(this->root, val); }
 };
 
-}
-#endif //STREAMINGCC_TREAP_H
+}  // namespace streamingcc
+#endif  // SRC_STREAMINGCC_INCLUDE_TREAP_H_
--- tests/treap_test.cc
+++ tests/treap_test.cc
@@ -1,3 +1,5 @@
+// Copyright 2019 Yan Song
+
 #include "../src/streamingcc_include/treap.h"
 #include <ctime>
 #include <cstdlib>
